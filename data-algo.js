fd81c13e3f56fe8e1e62334adf40b090361036d7
Henry Efamba Jr <efambajr.henry@gmail.com>
2023-10-04 19:20:25 -0500

    pointers

--- data-algo.js
+++ data-algo.js
@@ -1,50 +1,23 @@
-// function anagram(str1, str2) {
-//   if (str1.length !== str2.length) {
-//     return false;
-//   }
+function sumOfZero(arr) {
+  let sorted = arr.sort((a, b) => {
+    return a - b;
+  });
 
-//   let charCount1 = {};
-//   let charCount2 = {};
+  let left = 0;
+  let right = sorted.length - 1;
 
-//   for (let val of str1) {
-//     charCount1[val] = (charCount1[val] || 0) + 1;
-//     console.log(charCount1);
-//   }
-
-//   for (let val of str2) {
-//     charCount2[val] = (charCount2[val] || 0) + 1;
-//     console.log(charCount2);
-//   }
-
-//   for (let key in charCount1) {
-//     if (!(key in charCount2)) {
-//       return false;
-//     }
-//     if (charCount2[key] !== charCount1[key]) return false;
-//   }
-//   return true;
-// }
-
-function anagram(str1, str2) {
-  if (str1.length !== str2.length) {
-    return false;
+  while (left < right) {
+    let sum = sorted[left] + sorted[right];
+    if (sum < 0) {
+      left++;
+    } else if (sum > 0) {
+      right--;
+    } else {
+      return [sorted[left], sorted[right]];
+    }
   }
-
-  let lookup = {};
-
-  for (let i = 0; i < str1.length; i++) {
-    let letter = str1[i];
-    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
-    console.log(lookup);
-  }
-
-  for (let i = 0; i < str2.length; i++) {
-    let letter = str2[i];
-    lookup[letter] ? (lookup[letter] -= 1) : false;
-    console.log(lookup);
-  }
-  return true;
+  return 'nothing is zero';
 }
 
-let results = anagram('rracr', 'rrrac');
-console.log(results);
+let result = sumOfZero([-1, 10, 11, -2, 0, -4, -8]);
+console.log(result);
