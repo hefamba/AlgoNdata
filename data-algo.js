dba59b65d8bdb4eb4e76d5b408e258648aaaaa46
Henry Efamba Jr <efambajr.henry@gmail.com>
2023-10-05 20:48:57 -0500

    recursion

--- data-algo.js
+++ data-algo.js
@@ -1,18 +1,7 @@
-function maxSubArray(arr, num) {
-  if (arr.length < num) return null;
-  let maxNum = 0;
-  let tempNum = 0;
-
-  for (let i = 0; i < num; i++) {
-    maxNum += arr[i];
-  }
-  tempNum = maxNum;
-  for (let i = num; i < arr.length; i++) {
-    tempNum = tempNum - arr[i - num] + arr[i];
-    maxNum = Math.max(maxNum, tempNum);
-  }
-  return maxNum;
+function factorial(num) {
+  if (num === 1) return 1;
+  return num * factorial(num - 1);
 }
 
-let res = maxSubArray([1, 3, 22, 2, 22, 7, 4, 3, 2, 50, 1, 1, 9, 10], 3);
+let res = factorial(2);
 console.log(res);
