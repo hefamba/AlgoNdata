45e142003179096de46680d7eefa80506383ea7c
Henry Efamba Jr <efambajr.henry@gmail.com>
2023-10-17 15:15:52 -0500

    update

--- data-algo.js
+++ data-algo.js
@@ -1,13 +1,17 @@
-var getConcatenation = function (nums) {
-  let ans = [];
-  for (let i = 0; i < nums.length; i++) {
-    ans.push(nums[i]);
-  }
-  for (let j = 0; j < nums.length; j++) {
-    ans.push(nums[j]);
-  }
-  return ans;
-};
+var replaceElements = function (arr) {
+  let max = arr[arr.length - 1];
+  arr[arr.length - 1] = -1;
 
-let res = getConcatenation([1, 2, 1]);
+  for (let i = arr.length - 2; i >= 0; i--) {
+    let curr = arr[i];
+    arr[i] = max;
+    if (curr > max) {
+      max = curr;
+    }
+  }
+  return arr;
+};
+let res = replaceElements([17, 18, 5, 4, 6, 1]);
 console.log(res);
+
+// need to review this one more.
