434b00861f222afaa5c8c4fe7492f71dc4f97caa
Henry Efamba Jr <efambajr.henry@gmail.com>
2023-11-29 18:45:55 -0600

    new codesss

--- data-algo.js
+++ data-algo.js
@@ -1,9 +1,24 @@
-var lengthOfLastWord = function (s) {
-  let result = s.trim().split(' ');
-  let last = result.length - 1;
-  console.log(`index of ${last}`);
-  return console.log(`length of ${result[last].length}`);
+var longestCommonPrefix = function (strs) {
+  // Assume the prefix is the first word in the array
+  let pre = strs[0];
+  console.log(pre);
+
+  // Iterate through each word in the array
+  for (let word of strs) {
+    // Iterate through each character in the current word
+    console.log(word);
+    for (let i = pre.length - 1; i >= 0; i--) {
+      // If the characters do not match, update the prefix by removing the differing characters
+      if (pre[i] !== word[i]) {
+        pre = pre.slice(0, i);
+        console.log(pre);
+      }
+    }
+  }
+
+  // Return the final common prefix
+  return console.log(pre);
 };
 
-let done = lengthOfLastWord('Hello World all day boy');
+let done = longestCommonPrefix(['flower', 'flow', 'flight']);
 console.log(done);
