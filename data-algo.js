dbe55525a4af3d26b809071f905ad1ad1f3acb3e
Henry Efamba Jr <efambajr.henry@gmail.com>
2023-12-19 19:59:49 -0600

    new q

--- data-algo.js
+++ data-algo.js
@@ -1,13 +1,23 @@
-var twoSum = function (nums, target) {
-  let mp = {};
+var topKFrequent = function (nums, k) {
+  if (nums.length === 0 || nums.length === 1) return nums;
+
+  let count = {};
+  let res = [];
 
   for (let i = 0; i < nums.length; i++) {
-    let diff = target - nums[i];
-
-    if (diff in mp) {
-      return [i, mp[diff]];
+    let el = nums[i];
+    if (!count[el]) {
+      count[el] = 1;
+    } else {
+      count[el]++;
     }
-
-    mp[nums[i]] = i;
   }
+
+  let sorted = Object.keys(count).sort((a, b) => count[b] - count[a]);
+
+  for (let i = 0; i < k; i++) {
+    res.push(sorted[i]);
+  }
+
+  return res;
 };
